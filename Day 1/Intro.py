3f278eb2a217e04735c04ed9176ffd2a5bd30419
Aastha Khadake <jaibhavanipapercompany@gmail.com>
2026-07-02 13:47:30 +0530

    Create Intro.py

---
+++ Day 1/Intro.py
@@ -0,0 +1,24 @@
+#Introduction to Python
+#Day 1 Python
+print("Hello, World!")
+
+print(2 + 3)
+print(5 - 2)
+print(5 * 10)
+print(10 / 2)
+print(100 // 3)
+print(3 ** 2)
+print(3 % 2)
+
+
+print(type(10))
+print(type(10.5))
+print(type("Hello"))
+print(type(True))
+print(type(5 + 3j))
+print(type([1, 2, 3]))
+print(type((1, 2, 3)))
+print(type({"name": "John", "age": 30}))
+print(type({1, 2, 3}))
+print(type(None))
+print(type(lambda x: x + 1))
