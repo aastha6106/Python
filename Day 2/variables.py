7dec8f4d1abc3bce13446a5e9230543bf60085e4
Aastha Khadake <jaibhavanipapercompany@gmail.com>
2026-07-02 13:59:38 +0530

    Create variables.py

---
+++ Day 2/variables.py
@@ -0,0 +1,27 @@
+#Day 2: 30 days of pyhton programming
+
+first_name = "Aastha"
+last_name = "Aggarwal"
+full_name = first_name + " " + last_name 
+country = "India"
+city = "Mumbai"
+age = 19
+year = 2006
+is_married = False
+is_true = True
+is_light_on = True
+college_year, is_graduated, is_parent = 2, False, False
+
+print("First Name:", first_name) 
+print("Last Name:", last_name)
+print("Full Name:", full_name)
+print("Country:", country)
+print("City:", city)
+print("Age:", age)
+print("Year:", year)
+print("Is Married:", is_married)
+print("Is True:", is_true)
+print("Is Light On:", is_light_on)
+print("College Year:", college_year)
+print("Is Graduated:", is_graduated)
+print("Is Parent:", is_parent)
